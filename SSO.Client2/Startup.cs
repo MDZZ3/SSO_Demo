a74868c15e12a3f645655efe5727a08a2685ee58
MDZZ <Wen970517862@163.com>
2019-05-31 10:22:28 +0800

    删除了关于SSO_Demo对Redis的引用

--- SSO.Client2/Startup.cs
+++ SSO.Client2/Startup.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using SSO.Service.Services.RedisService;
 
 namespace SSO.Client2
 {
